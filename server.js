13c7a6de70a93da0369c919ab1c98a1fe9456c6f
Pfoersich <melocotonero14@gmail.com>
2026-05-13 15:13:18 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
+const express = require("express");
+const app = express();
+
 const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("MC Link Backend läuft");
+});
 
 app.listen(PORT, () => {
   console.log("Server läuft auf Port " + PORT);
